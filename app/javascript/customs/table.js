a781d3784003ceb2f240fba62bd846124f5b2b1f
t.kanin <tan.kanin98@gmail.com>
2021-09-15 12:09:37 +0700

    add sorting by header

--- app/javascript/customs/table.js
+++ app/javascript/customs/table.js
@@ -5,4 +5,21 @@ $(document).on('turbolinks:load', function(){
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
+
+  $('th').click(function(){
+    var table = $(this).parents('table').eq(0)
+    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+    this.asc = !this.asc
+    if (!this.asc){rows = rows.reverse()}
+    for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+  })
 })
+
+function comparer(index) {
+  return function(a, b) {
+      var valA = getCellValue(a, index), valB = getCellValue(b, index)
+      return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+  }
+}
+
+function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
