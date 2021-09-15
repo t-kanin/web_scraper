ac826b08ca8b5041d945fe79e89f6c0f81288c70
t.kanin <tan.kanin98@gmail.com>
2021-09-15 17:09:25 +0700

    retrieve the total search result

--- app/services/scraper_service.rb
+++ app/services/scraper_service.rb
@@ -4,22 +4,19 @@ require 'selenium-webdriver'
 require 'nokogiri'
 
 class ScraperService < ApplicationService
-  attr_reader :driver, :wait
+  attr_reader :driver, :wait, :keyword
 
-  def initialize
+  def initialize(keyword)
     @driver = Selenium::WebDriver.for :chrome
     @wait = Selenium::WebDriver::Wait.new(timeout: 10)
-
+    @keyword = keyword
+    driver.get 'http://www.google.com/'
   end
 
   def call
-    begin
-      driver.get 'https://google.com/ncr'
-      driver.find_element(name: 'q').send_keys 'cheese', :return
-      first_result = wait.until { driver.find_element(css: 'h3') }
-      puts first_result.attribute('textContent')
-    ensure
-      driver.quit
-    end
+    driver.find_element(name: 'q').send_keys keyword, :return
+    total_search = wait.until { driver.find_element(id: 'result-stats').text }
+    puts total_search
+    driver.quit
   end
 end
