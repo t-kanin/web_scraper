d14ae1835fee7df1d0efdebeba5885cf38389c21
t.kanin <tan.kanin98@gmail.com>
2021-09-15 10:48:13 +0700

    adding table + search function

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -4,4 +4,9 @@ class KeywordsController < ApplicationController
   def index
     @keywords = Keyword.all
   end
+
+  def import
+    Keyword.import(params[:file])
+    redirect_to root_url, notice: 'Successfully imported data.'
+  end
 end
