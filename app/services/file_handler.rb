f73ca77be404d446c678730183315c7eedd6214e
t.kanin <tan.kanin98@gmail.com>
2021-09-17 18:30:54 +0700

    check file extension

--- app/services/file_handler.rb
+++ app/services/file_handler.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class FileHandler
   class FileExntensionError < StandardError; end
 
@@ -16,7 +17,7 @@ class FileHandler
 
   def self.handle_upload(file, uid)
     raise FileEmptyError if file.nil?
-    raise FileExntensionError unless %w[csv].include?(file.extension)
+    raise FileExntensionError unless %w[.csv].include? File.extname(file)
     raise FileTooBigError if file.size > 1000.megabytes
 
     Keyword.import(file, uid)
