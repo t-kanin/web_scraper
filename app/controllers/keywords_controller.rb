4c31e6920482939318d6a208f036d1fb572f6445
t.kanin <tan.kanin98@gmail.com>
2021-09-20 09:30:04 +0700

    Update keywords_controller.rb

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class KeywordsController < ApplicationController
+  rescue_from FileHandler::FileExntensionError, with: :file_invalid
+  rescue_from FileHandler::FileEmptyError, with: :file_invalid
+  rescue_from FileHandler::FileTooBigError, with: :file_invalid
+
   def index
     @keywords = current_user.keywords
   end
@@ -9,15 +13,15 @@ class KeywordsController < ApplicationController
     file = params[:file]
     FileHandler.handle_upload(file, current_user.id)
     redirect_to root_url, notice: 'Successfully imported data.'
-  rescue FileHandler::FileExntensionError => e
-    redirect_to root_url, alert: e.message
-  rescue FileHandler::FileEmptyError => e
-    redirect_to root_url, alert: e.message
-  rescue FileHandler::FileTooBigError => e
-    redirect_to root_url, alert: e.message
   rescue SmarterCSV::MissingHeaders
     redirect_to root_url, alert: 'Missing require headers'
   rescue ActiveModel::UnknownAttributeError
     redirect_to root_url, alert: 'Headers not match'
+  end
+
+  private
+
+  def file_invalid(exception)
+    redirect_to root_url, alert: exception.message
   end
 end
