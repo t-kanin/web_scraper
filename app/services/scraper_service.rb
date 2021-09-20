892a8b51a5b666b7e0849ca50ba289a06b824464
t.kanin <tan.kanin98@gmail.com>
2021-09-20 17:37:57 +0700

    refactor and headless selenium option

--- app/services/scraper_service.rb
+++ app/services/scraper_service.rb
@@ -4,45 +4,46 @@ class ScraperService < ApplicationService
   include Singleton
 
   def call(keywords)
-    key_res = []
-    driver = init_driver
-    keywords.each do |keyword|
-      res = result(driver, keyword)
-      driver.find_element(name: 'q').clear
-      key_res << res
+    init_driver
+    result = keywords.each_with_object([]) do |keyword, accum|
+      res = result(keyword)
+      @driver.find_element(name: 'q').clear
+      accum << res
     end
 
-    driver.quit
-    key_res
+    @driver.quit
+    result
   end
 
   def init_driver
-    driver = Selenium::WebDriver.for :chrome
-    driver.get 'http://www.google.com/'
-    driver
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless')
+    @driver = Selenium::WebDriver.for :chrome, options: options
+    @driver.get 'http://www.google.com/'
+    @driver
   end
 
-  def result(driver, keyword)
-    driver.find_element(name: 'q').send_keys keyword, :return
-    doc = Nokogiri::HTML(driver.page_source)
+  def result(keyword)
+    @driver.find_element(name: 'q').send_keys keyword, :return
+    @dic = Nokogiri::HTML(@driver.page_source)
     {
-      page_result: page_result(doc),
-      search_result: search(doc, 'div.g', 'h3', 'a @href'),
-      ad_result: search(doc, 'div#tads a', 'span', '@href')
+      page_result: page_result,
+      search_result: fetch_non_adwords_links,
+      ad_result: fetch_top_position_adwords_links
     }
   end
 
-  def search(doc, div, title, link)
-    res = []
-    elements = doc.css(div)
-    elements.each do |e|
-      res << { title: e.css(title).text, link: e.css(link).first.text } unless e.css(title).blank?
-    end
-
-    res
+  def fetch_top_position_adwords_links
+    @dic.css('div#tads a').map { |link| { title: link.css('span').text, link: link.css('@href').first.text } }
+        .reject { |h| h[:title].empty? }
   end
 
-  def page_result(doc)
-    doc.at_css('div#result-stats').text
+  def fetch_non_adwords_links
+    a = @dic.css('div.g').map { |link| { title: link.css('h3').text, link: link.css('a @href').first.text } }
+            .reject { |h| h[:title].empty? }
+  end
+
+  def page_result
+    @dic.at_css('div#result-stats').text
   end
 end
